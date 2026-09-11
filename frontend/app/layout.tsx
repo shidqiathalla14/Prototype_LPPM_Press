b8b8e53458a2cc7631ba5249c7c8df475dec714b
Shidqi Athalla <shidqiathalla1406@gmail.com>
2026-09-11 11:09:51 +0700

    chore-clean-unused-frontend-asset

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="id" suppressHydrationWarning>
-      <body suppressHydrationWarning>
+      <body suppressHydrationWarning> 
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
